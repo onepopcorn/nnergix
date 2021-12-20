0c80021f8e647691046474f477e35281b01781d1
onepopcorn <marc.feliu@gmail.com>
2021-12-19 22:36:24 -0700

    Fix: Apps now takes into account async data loaded

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -1,7 +1,8 @@
-import { render } from '@testing-library/react'
+import { render, screen } from '@testing-library/react'
 
 import App from '../App.jsx'
 
-test('App should mount without crashing', () => {
+test('App should mount without crashing', async () => {
     render(<App />)
+    await screen.findByAltText('NNergix')
 })
