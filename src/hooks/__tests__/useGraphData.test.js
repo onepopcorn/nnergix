92450d2517c2d22dbff7a42cadcdc69a040dd28d
onepopcorn <marc.feliu@gmail.com>
2021-12-20 11:29:49 -0700

    New: graph hook can add points & reset to the default values

--- src/hooks/__tests__/useGraphData.test.js
+++ src/hooks/__tests__/useGraphData.test.js
@@ -4,10 +4,10 @@ import useGraphData from '../useGraphData.js'
 test('useGraphData should load default values', async () => {
     const test_data = {
         layout: 'layout-stub',
-        data: 'data-stub',
+        data: [{ x: [], y: [] }],
     }
     fetch.mockResponseOnce(JSON.stringify(test_data));
-    const { result, waitForNextUpdate } = renderHook(() => useGraphData('endpoint'))
+    const { result, waitForNextUpdate } = renderHook(() => useGraphData('testid'))
 
     // loading
     expect(result.current.loading).toBe(true)
@@ -21,8 +21,42 @@ test('useGraphData should load default values', async () => {
 
 test('useGraphData should return error when endpoint is not reached', async () => {
     fetch.mockReject(() => Promise.reject('file not found'))
-    const { result, waitForNextUpdate } = renderHook(() => useGraphData('endpoint'))
+    const { result, waitForNextUpdate } = renderHook(() => useGraphData('testid'))
 
     await waitForNextUpdate()
     expect(result.current.error).toBe(true)
+})
+
+test('useGraphData should be able to add points', async () => {
+    const test_data = {
+        layout: 'layout-stub',
+        data: [{
+            x: [1, 2, 3],
+            y: [1, 2, 3],
+        }],
+    }
+    fetch.mockResponseOnce(JSON.stringify(test_data));
+    const { result, waitForNextUpdate } = renderHook(() => useGraphData('testid'))
+    await waitForNextUpdate()
+
+    // default data
+    expect(result.current.data[0].x).toEqual(test_data.data[0].x)
+    expect(result.current.data[0].y).toEqual(test_data.data[0].y)
+
+    // add data
+    act(() => {
+        result.current.addPoints(4, 4)
+    })
+
+    expect(result.current.data[0].x).toEqual([...test_data.data[0].x, 4])
+    expect(result.current.data[0].y).toEqual([...test_data.data[0].y, 4])
+
+    // reset data
+    act(() => {
+        result.current.resetPoints()
+    })
+
+    expect(result.current.data.x).toEqual(test_data.data.x)
+    expect(result.current.data.y).toEqual(test_data.data.y)
+
 })
--- src/hooks/useGraphData.js
+++ src/hooks/useGraphData.js
@@ -7,6 +7,7 @@ const useGraphData = (id = null) => {
     const [data, setData] = useState(null)
     const [loading, setLoading] = useState(true)
     const [error, setError] = useState(false)
+    const [addedIndex, setAddedIndex] = useState(-1)
 
     const loadData = async (controller) => {
         try {
@@ -14,11 +15,39 @@ const useGraphData = (id = null) => {
             const res = await getGraphData(id, controller)
             setLayout(res.layout)
             setData(res.data)
+            setAddedIndex(res.data[0].x.length)
         } catch (e) {
             setError(true)
         } finally {
             setLoading(false)
         }
+    }
+
+    /**
+     * NOTE: For the purpose of this test we will only 
+     * care about the first chart plot but in a real worl app
+     * we should care about all possible charts
+     * 
+     */
+    const addPoints = (x, y) => {
+        setData([
+            {
+                ...data[0],
+                x: [...data[0].x, x],
+                y: [...data[0].y, y],
+            }
+        ])
+
+    }
+
+    const resetPoints = () => {
+        setData([
+            {
+                ...data[0],
+                x: data[0].x.slice(0, addedIndex),
+                y: data[0].y.slice(0, addedIndex),
+            }
+        ])
     }
 
     useEffect(() => {
@@ -30,7 +59,7 @@ const useGraphData = (id = null) => {
         return () => controller.abort()
     }, [id])
 
-    return { loading, error, layout, data }
+    return { loading, error, layout, data, addPoints, resetPoints }
 }
 
 export default useGraphData
