8c4cf12795faccf426ec2e12f3012a02658b789d
onepopcorn <marc.feliu@gmail.com>
2021-12-20 15:34:08 -0700

    Refactor: modified app to show the new dashboard

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,7 @@
 import style from './App.module.css'
 import Header from './components/layout/Header.jsx'
 import SideMenu from './components/layout/SideMenu.jsx'
-import WidgetArea from './components/layout/WidgetArea.jsx'
-import Widget from './components/layout/Widget.jsx'
+import Dashboard from './components/Dashboard.jsx'
 
 function App() {
 
@@ -41,16 +40,7 @@ function App() {
         <div className={style.app}>
             <Header />
             <SideMenu items={sidemenu} />
-            <WidgetArea title='Dashboard' icon='bar_chart' >
-                <Widget title="Statistics">
-                    <p>This is a small text</p>
-                    <p>this is a second text</p>
-                </Widget>
-                <Widget title="Other panels">
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque fermentum ex ac nibh elementum dapibus. Nam ac eleifend metus. In hac habitasse platea dictumst. Suspendisse et fringilla orci. Vivamus non felis eu purus accumsan sollicitudin. Proin cursus, est ac hendrerit aliquam, diam nunc pulvinar magna, sagittis tincidunt ante felis sit amet odio. In dui justo, condimentum in rhoncus sed, suscipit non arcu. Donec vel finibus libero. Sed consectetur orci sed dolor gravida luctus. Vivamus ligula ipsum, tincidunt ac laoreet et, facilisis et purus. Fusce ultricies et felis imperdiet ultrices. Sed pretium odio eget dui auctor rutrum. Maecenas et urna vel purus auctor sollicitudin quis at mauris. Quisque tincidunt faucibus varius. Nullam eget varius justo, eu malesuada turpis.</p>
-                </Widget>
-
-            </WidgetArea>
+            <Dashboard />
         </div >
     )
 }
