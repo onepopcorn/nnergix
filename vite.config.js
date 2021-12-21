b2b435b5d0da0bf1ec4f3584ba5ac17e2c55774f
onepopcorn <marc.feliu@gmail.com>
2021-12-21 08:28:24 -0700

    Fix: updated vite config and api prefix to prevent issue with the test environment

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/nnergix/',
-  plugins: [react()]
+  plugins: [react()],
+  define: {
+    "process.env.NODE_ENV": `"${process.env.NODE_ENV}"`,
+    "process.env.APP_BASE_URL": `"${process.env.NODE_ENV === 'production' ? '/nnergix' : ''}"`
+  },
 })
